09e1193331810e9dd5ee100279d1d437f5945062
Oliulla <mohammad.oliullah25@gmail.com>
2025-03-13 13:20:49 +0600

    Added log

--- services/data-fetcher/fetcher.c
+++ services/data-fetcher/fetcher.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <time.h>
+
+// Log function
+void log_message(const char *message) {
+    FILE *log_file = fopen("fetcher.log", "a");
+    if (log_file) {
+        time_t now;
+        time(&now);
+        fprintf(log_file, "%s - %s\n", ctime(&now), message);
+        fclose(log_file);
+    }
+}
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return fwrite(ptr, size, nmemb, stream);
@@ -21,6 +33,14 @@ int main() {
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         fclose(fp);
+        
+        if (res == CURLE_OK) {
+            log_message("Successfully fetched stock data and saved to stock_data.json");
+        } else {
+            log_message("Error fetching stock data");
+        }
+    } else {
+        log_message("Error initializing cURL");
     }
 
     printf("Stock data saved to %s\n", outfilename);
